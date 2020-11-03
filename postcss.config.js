fcc3b49da1d59535fb9ee240b42e4e6f4eeb3393
randrerd <raulromand@gmail.com>
2020-11-02 21:34:43 -0300

    :fire: chore: PostCSS purge

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,9 @@ module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
     require('autoprefixer'),
-    // require('@fullhuman/postcss-purgecss')({
-    //   content: ['./src/**/*.js', './public/index.html'],
-    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    // }),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.js', './public/index.html'],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
 };
