866082d92cc62b67a5cab82c3a219763fa77ec69
randrerd <raulromand@gmail.com>
2020-11-10 13:34:37 -0300

    :lipstick: style: Minor style changes
    -Adds heading styling to Projects Section heading
    -Modifies UX writing for 'Repo' button
    -Modifies text color for better accesibility

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -14,20 +14,20 @@ export default function Project(props) {
   const { handleClick } = props;
 
   return (
-    <div className='flex flex-col items-center my-12 lg:flex-row lg:mb-32'>
+    <div className='flex flex-col my-12 lg:flex-row lg:mb-32'>
       <div className={`flex flex-col mb-4 lg:mx-48 lg:max-w-1xl lg:mr-20 lg:mb-0`}>
     
-        <h4 className={'text-white mb-3 lg:self-start font-light lg:mt-3 lg:mb-0'}>{title}</h4>
+        <h4 className={'text-white mb-3 font-normal lg:self-start lg:mt-3 lg:mb-0'}>{title}</h4>
         <button onClick={() => handleClick(secondLink)}>
           <Preview previewUrl={previewUrl} />
         </button>
        
-        <div className={'flex justify-center w-full'}>
+        <div className={'flex justify-center w-full max-w-sm self-center lg:max-w-none'}>
           <button
             onClick={() => handleClick(firstLink)}
-            className={'w-auto flex-grow h-8 bg-white mx-2 lg:w-24 lg:h-8 lg:flex-grow-0'}
+            className={'w-auto flex-grow h-8 bg-white mx-2 lg:w-24 lg:h-8 lg:flex-grow-0 max'}
           >
-            <span>Repo</span>
+            <span>Code</span>
           </button>
           <button
             onClick={() => handleClick(secondLink)}
@@ -41,7 +41,7 @@ export default function Project(props) {
       
       </div>
       <div className='flex flex-col items-start flex-shrink-0 lg:mt-19 lg:w-1/4'>
-      <p className={'text-whiteish'}>{summary}</p>
+      <p className={'text-white text-lg font-light'}>{summary}</p>
       <TechStack content={techStack} />
       </div>
     </div>
--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -3,13 +3,26 @@ import projects from '../../data/projects.json';
 import Project from '../Project';
 
 export default function ProjectsSection(props) {
-
+ 
 
   return (
-    <div className={`bg-black px-8 py-4 pb-24 mt-7 flex flex-col lg:px-48 lg:py-16`}>
-      <h3 className={`text-white font-normal center m-auto mb-3 mt-2 lg:m-0`}>Projects</h3>
+    <div
+      className={`bg-black px-8 py-4 pb-24 mt-7 flex flex-col lg:px-48 lg:py-16`}
+    >
+      <div className="flex my-5 mb-3 lg:mt-0">
+        <h3 className={`relative text-white z-20 blend-difference lg:text-5xl`}>Projects</h3>
+        <div
+          className={`relative self-center right-32 lg:right-48 w-16 h-8 lg:h-12 bg-white`}
+        />
+      </div>
       {projects.map((project, key) => {
-        return <Project details={project} key={key} handleClick={props.handleClick}/>;
+        return (
+          <Project
+            details={project}
+            key={key}
+            handleClick={props.handleClick}
+          />
+        );
       })}
     </div>
   );
