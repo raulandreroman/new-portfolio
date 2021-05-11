ba6058ba4fe64131633cc1d51a0d5c7937359809
Raul <raul@superherocheesecake.com>
2021-05-11 18:43:28 -0300

    Remove unused var

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './styles/main.css';
-import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { ThemeProvider } from './containers/ThemeContext';
 import { LanguageProvider } from './containers/Language';
